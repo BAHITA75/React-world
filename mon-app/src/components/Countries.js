2d712f12a3fccb42e0c121ac50f902993a513599
Takabait <a.takabait@gmail.com>
2022-01-23 22:46:46 +0100

    button filter

--- mon-app/src/components/Countries.js
+++ mon-app/src/components/Countries.js
@@ -5,39 +5,31 @@ import Card from './Card';
 const Countries = () => {
 
     const [data, setData] = useState([]);
-    const [sortedData, setSortedData] = useState([]);
-    const [playOnce, setPlayOnce] = useState(true);
-    const [rangeValue, setRangeValue] = useState(250);
+    const [rangeValue, setRangeValue] = useState(30000000);
     const [selectedRadio, setSelectedRadio] = useState('');
     const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
     const [searchInput, setSearchInput] = useState("");
+    const [isCrescent, setIsCrescent] = useState(false);
 
     useEffect(() => {
-        if (playOnce) {
-            axios.get("https://restcountries.com/v2/all?fields=name,population,flag,region,capital")
-                .then((res) => {
-                    setData(res.data)
-                    setPlayOnce(false);
-                });
-        }
 
-        const sortedCountry = () => {
-            const countryObj = Object.keys(data).map((i) => data[i]);
-            const sortedArray = countryObj.sort((a, b) => {
-                return b.population - a.population
-            });
-            sortedArray.length = rangeValue;
-            setSortedData(sortedArray);
-        }
-        sortedCountry();
-    }, [data, playOnce, rangeValue]);
+        axios.get("https://restcountries.com/v2/all?fields=name,population,flag,region,capital")
+            .then((res) => setData(res.data));
+    }, []);
+
+    const numberFormat = (num) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    };
 
 
     return (
         <div className="countries">
             <div className='sort-container'>
-                <input type="range" min="1" max="250" value={rangeValue}
+                <input type="range" min="0" max="1377482166" defaultvalue={rangeValue}
                     onChange={(e) => setRangeValue(e.target.value)} />
+                <p style={{ marginBottom: "20px" }}>
+                    {numberFormat(rangeValue)} habitants
+                </p>
 
                 <ul className='radio-container'>
                     {radios.map((radio) => {
@@ -49,24 +41,40 @@ const Countries = () => {
                             </li>
                         )
                     })}
+                    <div className='cancel'>
+                        {selectedRadio && <h5 onClick={() => setSelectedRadio("")}>
+                            Annuler la recherche
+                        </h5>}
+                    </div>
                 </ul>
             </div>
 
-            <div className='cancel'>
-                {selectedRadio && <h5 onClick={() => setSelectedRadio("")}>
-                    Annuler la recherche
-                </h5>}
+            <div className='filter'>
+                {isCrescent ? (
+                    <button onClick={() => setIsCrescent(false)}>Tri décroissant <br/><strong>par population</strong></button>
+                ) : (
+                    <button onClick={() => setIsCrescent(true)}>Tri croissant<br/><strong>par population</strong></button>
+                )}
+
+                <input
+                    type="text"
+                    placeholder="Entrez le nom d'un pays (en anglais)"
+                    onChange={(e) => setSearchInput(e.target.value)}
+                />
             </div>
-            
-            <input
-                type="text"
-                placeholder="Entrez le nom d'un pays (en anglais)"
-                onChange={(e) => setSearchInput(e.target.value)}
-            />
+
             <ul className='countries-list'>
-                {sortedData
+                {data
                     .filter((country) => country.region.includes(selectedRadio))
+                    .filter((country) => country.population > rangeValue)
                     .filter((country) => country.name.toLowerCase().includes(searchInput.toLowerCase()))
+                    .sort((a, b) => {
+                        if (isCrescent) {
+                            return a.population - b.population;
+                        } else {
+                            return b.population - a.population;
+                        }
+                    })
                     .map((country) => (
                         <Card country={country} key={country.name} />
                     ))}
